ab8ea17094b4a035b59148aab81693d324c7bb24
Crusherok <elroypinto84@gmail.com>
2024-07-16 15:06:54 +0530

    hi

--- index.js
+++ index.js
@@ -28,14 +28,81 @@
 //  }
 // let a='hey';
 // console.log(`abc ${a}`)
-function func(){
-    console.log("hey there!")
-}
-func();
-document.addEventListener("click",()=>{
-   console.log("djbf")
-})
-const fun3=()=>{
+// function func(){
+//     console.log("hey there!")
+// }
+// func();
+// document.addEventListener("click",()=>{
+//    console.log("djbf")
+// })
+// const fun3=()=>{
 
-}
-fun3()
+// }
+// fun3()
+// function delay(item){
+//    return item*5;
+// }
+// const result=delay(300);
+// console.log(result);
+
+
+// let arr=[1,2,3,4,5,]
+// for(let i in arr){
+//    console.log(i)
+// }
+// const obj={
+//    name:'ABC',
+//    age:34,
+//    section:'A'
+// }
+// for(let i in obj){
+//    console.log(`Key :${i} \t value:${obj[i]}`)
+// }
+// let a=1;
+
+// while(a<=10){
+//    console.log("iterated ",a,"times")
+//    a++;
+// }
+// i=1;
+// do{
+//    console.log("value",i)
+//    i++;
+// }while(i<=5);
+// let a=[1,2,3,4]
+// a.map((value,index)=>{
+//    console.log(`${index}`,`${value}`)
+// })
+// let a=[1,2,3,4]
+//  a.forEach((value,index)=>{
+//    console.log(`${index}`,`${value}`)
+//  })
+// function getEvenNum(arr){
+//    let evenNum=[];
+//    for(let i=0;i<arr.length;i++){
+//       if(arr[i]%2==0){
+//          evenNum.push(arr[i]);
+//       }
+//    }
+//    return evenNum;
+// }
+// let numbers=[1,2,3,4,5,6,7,8,9,10];
+// let result=getEvenNum(numbers);
+// console.log(result);
+// let p=new Promise((ressolve,reject)=>{
+//    let a=10;
+//    if(a>=18){
+//       ressolve("habibi you turned adult");
+//    }else{
+//       reject("habibi under water");
+//    }
+// })
+//p.then((m)=>{
+  // console.log(m);
+//})
+// p.catch((e)=>{
+//    console.log(e);
+// })
+// setInterval(()=>{
+//    console.log("Hey there");
+// },3000)

f5397ca5ff34e4402d3f80d94fe53a694fcf81ae
Crusherok <elroypinto84@gmail.com>
2024-07-16 11:01:43 +0530

    gat

---
+++ index.js
@@ -0,0 +1,41 @@
+//let a=10;
+//let b=12.23
+//let c=true
+//console.log(typeof(a))
+//console.log(b)
+
+//console.log(c)
+//const obj={
+   // name:"abs",
+   // age:15
+//}
+//console.log(obj)
+//let arr=[1,2,3,4,5,6,7]
+//console.log(arr)
+// let a=2;
+// if(a){
+//     console.log("True value")
+// }else if(a==2){
+//     console.log("NEITHER TRUE NOR FALSE its just a number")
+// }else{
+//     console.log("False")
+// }
+// let age=15;
+// if(age>=18){
+//     console.log("Above 18")
+// }else{
+//     console.log("Below 18")
+//  }
+// let a='hey';
+// console.log(`abc ${a}`)
+function func(){
+    console.log("hey there!")
+}
+func();
+document.addEventListener("click",()=>{
+   console.log("djbf")
+})
+const fun3=()=>{
+
+}
+fun3()
